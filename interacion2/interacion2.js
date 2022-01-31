1e2cdf18cbd80e13314de824f02f97357c86b241
alejandro martin rodriguez <thalitaa@hotmail.com>
2022-01-31 16:15:42 +0000

    reviewbasis

---
+++ interacion2/interacion2.js
@@ -0,0 +1,79 @@
+/* **Iteración #2: Mix Fors**
+
+Dado el siguiente javascript usa forof y forin para hacer la media del volumen de todos los sonidos favoritos que 
+tienen los usuarios.
+
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+``` */
+
+const users = [
+    {name: 'Manolo el del bombo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 50},
+            rain: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'Mortadelo',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 30},
+            shower: {format: 'ogg', volume: 55},
+            train: {format: 'mp3', volume: 60},
+        }
+    },
+    {name: 'Super Lopez',
+        favoritesSounds: {
+            shower: {format: 'mp3', volume: 50},
+            train: {format: 'ogg', volume: 60},
+            firecamp: {format: 'mp3', volume: 80},
+        }
+    },
+    {name: 'El culebra',
+        favoritesSounds: {
+            waves: {format: 'mp3', volume: 67},
+            wind: {format: 'ogg', volume: 35},
+            firecamp: {format: 'mp3', volume: 60},
+        }
+    },
+]
+
+mediaVolume= 0;
+totalVolume= 0;
+for (user of users){
+    for(favoritesSound in user.favoritesSounds)    {        
+        mediaVolume+= (user.favoritesSounds[favoritesSound]).volume;
+        //mediaVolume = mediaVolume + (user.favoritesSounds[favoritesSound]).volume;
+        totalVolume++;
+    }        
+}
+console.log(mediaVolume/totalVolume);
---
+++ interacion4/interacion4.js
@@ -0,0 +1,39 @@
+/* **Iteración #4: Métodos findArrayIndex**
+
+Crea una función llamada `findArrayIndex` que reciba como parametros un array de textos y 
+un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que 
+enviaste como parametro. Haz varios ejemplos y compruebalos.
+
+Sugerencia de función:
+
+```jsx
+function findArrayIndex(array, text) {}
+```
+
+Ej array:
+
+```jsx
+['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
+```
+ */
+
+function findArrayIndex(array, text) {
+    for (let i = 0; i < array.length; i++) {
+      let obj = array[i];
+      if (obj === text) {
+        return i;
+      }
+    }
+  }
+  
+  const index = findArrayIndex(
+    ["Caracol", "Mosquito", "Salamandra", "Ajolote"],
+    "Ajolote"
+  );
+  console.log(index);
+  
+  const index2 = findArrayIndex(
+    ["Caracol", "Mosquito", "Salamandra", "Ajolote"],
+    "Mosquito"
+  );
+  console.log(index2);
